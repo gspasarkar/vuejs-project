758f74dd9fbdc9c8c26e45819d4dba1f3614df85
Ganesh Pasarkar <ganesh.pasarkar@gmail.com>
2023-10-01 13:24:29 +0530

    changes done

--- vue-formexample/src/components/HelloWorld.vue
+++ vue-formexample/src/components/HelloWorld.vue
@@ -1,113 +1,69 @@
+
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div id="app">
+      {{JSON.stringify(formdata,null,2) }}
+      <form name="regForm" @click="submitForm()">
+          <div>
+            <label for="firstName">First Name:</label>
+            <input id="firstName" value="" v-model.trim="formdata.firstName">
+          </div>
+          <div>
+           <label for="lastName">Last Name:</label>
+           <input id="lastName" value="" v-model="formdata.lastName">
+          </div>
+          <div>
+            <label for="age">Age:</label>
+            <input id="age" value="" v-model.number="formdata.age">
+          </div>
+          <div>
+            <label for="address">Address:</label>
+            <textarea id="address" v-model="formdata.address"></textarea>
+          </div>
+          <div>
+            <label for="country">Country:</label>
+            <select  name="countries"  v-model="formdata.country">
+              <option value="india" select>India</option>
+              <option value="usa">USA</option>
+              <option value="uk">UK</option>
+            </select>
+          </div>
+          <div>
+            <label for="city">City:</label>
+            <select  name="cities" v-model="selectedValue" >
+              <option v-for="(city,index) of formdata.cities" :key="index" :value="city"  >{{city}}</option>
+            </select>
+          </div>
+      </form>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+/* eslint-disable */
+    // add a default exported object export default {}, which is your component object.
+    // export default is used to create local registration for Vue component.
+    // The first case (export default {...}) is ES2015 syntax for making some object definition available for use.
+export default{
+  // the registered name of component.
+  name: "registration-form",
+    // Also, be sure to declare your data properties as functions, otherwise they are not going to be reactive.
+    // You'll note that the data property is a function. This is to keep the data values unique for each instance of a component at runtime — the function is invoked separately for each component instance. 
+    // If you declared data as just an object, all instances of that component would share the same values. This is a side-effect of the way Vue registers components and something you do not want.
+    data() {
+      return{
+        formdata:{
+          firstName: '',
+          lastName: '',
+          age: null,
+          address:'',
+          country:'',
+          cities:['Pune','Nagpur','Mumbai'],
+          selectedValue: 'Mumbai',
+        }
+      }
+    },
+    methods: {
+      submitForm(){
+          this.option.value = selected
+      }
     }
-  }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
---
+++ vue-formexample/src/components/form-example.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="app">
+        <form name="regForm" @click="submitForm()">
+            First Name: <input name="firstName" value="">
+
+        </form>
+    </div>
+</template>
+<script>
+export default({
+    
+})
+</script>
+
