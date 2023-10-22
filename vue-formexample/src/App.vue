3bc6ac54a9d65e51f0be149d615ede96019b69cb
Ganesh Pasarkar <ganesh.pasarkar@gmail.com>
2023-10-22 16:32:13 +0530

    basic-compoent-example

--- vue-formexample/src/App.vue
+++ vue-formexample/src/App.vue
@@ -1,23 +1,9 @@
 <template>
-<div>
-  <button @click="filmList.push('Kantara')">Add Film</button>
-</div>
+  <hello-world></hello-world>
 </template>
 <script>
+import HelloWorld from './components/HelloWorld'
 export default {
-  data () {
-    return {
-      filmList: ['Jailor', 'Jawaan']
-    }
-  },
-  watch: {
-    filmList: {
-      handler (newValue) {
-        console.log('Current Films:', newValue)
-      },
-      deep: true,
-      immediate: true
-    }
-  }
+  components: { HelloWorld }
 }
 </script>
---
+++ vue-formexample/src/components/HelloWorld.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Hello from a component</h1>
+</template>
